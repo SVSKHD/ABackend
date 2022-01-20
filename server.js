190266be99046c4188cc215f3744e4337a76f691
SVSKHD <8svskhd@gmail.com>
2022-01-20 11:53:45 +0530

    chnages push

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 
 // db
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.DATABASE_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
